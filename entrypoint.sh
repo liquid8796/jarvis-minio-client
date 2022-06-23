86d0e3976935c4cf6109395ddb48f64636a2ad2a
Fat Rat <60702632+liquid8796@users.noreply.github.com>
2022-06-23 20:10:48 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 cd /minio-client
 wget https://dl.min.io/client/mc/release/linux-amd64/mc --no-check-certificate
 chmod +x mc
-mc alias set minio https://jarvis-minio-server.tk FatRat_080796 Qwerty@010521@Bep
+./mc alias set minio https://jarvis-minio-server.tk FatRat_080796 Qwerty@010521@Bep
